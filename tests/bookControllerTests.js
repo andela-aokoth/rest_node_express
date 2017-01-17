b7d5863a35569d432e19934c8752e35e529faa54
Arnold Okoth <arnold.okoth@andela.com>
2017-01-17 14:08:30 +0300

    Add book controller unit tests

---
+++ tests/bookControllerTests.js
@@ -0,0 +1,28 @@
+var should = require('should');
+var sinon = require('sinon');
+
+describe('Book Controller Tests', function() {
+    describe('POST', function() {
+        it('should not allow an empty title on post', function() {
+            var Book = function(book) {
+                this.save = function() {};
+            };
+
+            var req = {
+                body: {
+                    author: 'Sidney Sheldon'
+                }
+            };
+
+            var res = {
+                status: sinon.spy(),
+                send: sinon.spy()
+            };
+
+            var bookController = require('../controllers/bookController')(Book);
+            bookController.post(req, res);
+            res.status.calledWith(400).should.equal(true, 'Bad Status ' + res.status.args[0][0]);
+            res.send.calledWith('Title is required').should.equal(true);
+        });
+    });
+});
