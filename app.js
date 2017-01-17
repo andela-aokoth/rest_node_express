6ed867e0e62ee0b6cebb8236a8d91eee5f383282
Arnold Okoth <arnold.okoth@andela.com>
2017-01-17 11:03:46 +0300

    Add application entry point

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+var app = express();
+var port = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var bookRouter = require('./routes/bookRoutes')();
+var authorRouter = require('./routes/authorRoutes')();
+app.use('/api/v0.1/books', bookRouter);
+app.use('/api/v0.1/authors', authorRouter);
+
+
+app.get('/', function(req, res) {
+    res.send({message: 'Welcome to the API'});
+});
+
+app.listen(port, function() {
+    console.log('Running on PORT: ' + port);
+});
